4049bb9e0c9899fbe3971da67215131038c5fa67
hwangyoomin <yoominh3@gmail.com>
2023-09-01 10:51:39 +0900

    Create Hello Project

---
+++ src/main/java/Hello.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Hello {
+    private String hno;
+    private String name;
+    
+    public String getHno() {
+        return hno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setHno(String hno) {
+        this.hno = hno;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public toString(){
+        
+    }
+}
