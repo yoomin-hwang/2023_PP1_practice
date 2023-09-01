e6c425b182a040851c376f79058b932fd014f50f
hwangyoomin <yoominh3@gmail.com>
2023-09-01 10:50:28 +0900

    Create Hello Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+//package org.example;
+//
+//// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+//// then press Enter. You can now see whitespace characters in your code.
+//public class Main {
+//    public static void main(String[] args) {
+//        // Press Alt+Enter with your caret at the highlighted text to see how
+//        // IntelliJ IDEA suggests fixing it.
+//        System.out.printf("Hello and welcome!");
+//
+//        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+//        for (int i = 1; i <= 5; i++) {
+//
+//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+//            // for you, but you can always add more by pressing Ctrl+F8.
+//            System.out.println("i = " + i);
+//        }
+//    }
+//}
+
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Student s1 = new Student("21900000", "홍길동");
+        System.out.println(s1.toString());
+    }
+}
